4fceae70d5ac1d170daefe3409940db18b06ffb0
TakeActionNow <bartkrav@gmail.com>
2015-10-12 21:29:35 +0200

    First commit
    First ever commit.
    SVG map

---
+++ Ronin/Ronin/Scripts/js/Directive.js
@@ -0,0 +1,30 @@
+﻿angular.module('SvgMapApp').directive('svgMap', ['$compile', function ($compile) {
+    return {
+        restrict: 'A',
+        templateUrl: '../../Content/img/worldHigh.svg',
+        link: function (scope, element, attrs) {
+            var regions = element[0].querySelectorAll('.land');
+            angular.forEach(regions, function (region, key) {
+                var regionElement = angular.element(region);
+                regionElement.attr("region", "");
+                $compile(regionElement)(scope);
+            });
+        }
+    }
+}])
+
+angular.module('SvgMapApp').directive('region', ['$compile', function ($compile) {
+    return {
+        restrict: 'A',
+        scope: true,
+        link: function (scope, element, attrs) {
+            scope.elementId = element.attr("id");
+            scope.regionClick = function () {
+                alert('ElementId: ' + scope.elementId + 'Element title: ' + element.attr("title") );
+            };
+            element.attr("ng-click", "regionClick()");
+            element.removeAttr("region");
+            $compile(element)(scope);
+        }
+    }
+}]);
